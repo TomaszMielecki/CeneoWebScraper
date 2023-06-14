54601a7f3a7250e560eb3e862c4a7d117d801051
TomaszMielecki <tomek.mielecki@gmail.com>
2023-06-14 16:31:45 +0200

    2023-06-14

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,11 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+import os
+import requests
+import pandas as pd
+import json
+from bs4 import BeautifulSoup
+from app.utils import extract_tag, selectors
 
 @app.route('/')
 @app.route('/index')
@@ -7,20 +13,49 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/extract')
+@app.route('/extract' , methods=['POST', 'GET'])
 def extract():
-    return render_template("extract.html")
+    if request.method == 'POST':
+        product_code = request.form.get('product_code')
+        url = f"https://www.ceneo.pl/{product_code}#tab=reviews"
+
+    all_opinions = []
+
+    while(url):
+        print(url)
+        response = requests.get(url)
+        print(response.status_code)
+        page_dom = BeautifulSoup(response.text, "html.parser")
+        opinions = page_dom.select("div.js_product-review")
+        
+
+        for opinion in opinions:
+            single_opinion = {}
+            for key, value in selectors.items():
+                single_opinion[key] = extract_tag(opinion, *value)
+            all_opinions.append(single_opinion)
+        try:
+            url = "https://www.ceneo.pl" + extract_tag(page_dom, "a.pagination__next", "href")
+        except TypeError:
+            url = None
+
+    with open(f"./opinions/{product_code}.json", "w", encoding="UTF-8") as jf:
+        json.dump(all_opinions, jf, indent=4, ensure_ascii=False)
+        return redirect(url_for('product', product_code=product_code))
+        return render_template("extract.html")
 
 
-@app.route('/product')
-def product():
-    return render_template("product.html")
+@app.route('/product/<product_code>')
+def product(product_code):
+    opinions =pd.read_json(f"{./app/data/opinions/{product_code}.json}")
+    return render_template("product.html", product_code=product_code
+    to_html(headers=True, classes='table table-striped table-success', table_id='opinions'))
 
 
 @app.route('/products')
 def products():
-    return render_template("products.html")
+    return render_template("products.html",)
 
 @app.route('/author')
-def products():
+def author():
     return render_template("author.html")
